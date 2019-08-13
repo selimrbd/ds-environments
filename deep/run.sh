5b73708e7bc95283843d9a0ed3402b6c80f334b5
Selim Raboudi <selim.raboudi@gmail.com>
2019-08-13 14:30:49 +0000

    add gpuus all to run

--- deep/run.sh
+++ deep/run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 docker run \
   -it \
+  --gpus all \
   -p 8888:__JUPYTER_NOTEBOOK_PORT_ON_HOST__ \
   --rm --name __CONTAINER_NAME__ \
   --mount type=bind,source="__PATH_HOST_WD__",target="__PATH_DOCKER_WD__" \
