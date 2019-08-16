3be8c9af200fe72afbf93d85f2ece393a1bc4ca0
Selim Raboudi <selim.raboudi@gmail.com>
2019-08-16 17:51:49 +0000

    add user command in docker run

--- basic/run.sh
+++ basic/run.sh
@@ -3,5 +3,6 @@ docker run \
   -it \
   -p 8888:__JUPYTER_NOTEBOOK_PORT_ON_HOST__ \
   --rm --name __CONTAINER_NAME__ \
+  --user __USER_ID__:__GROUP_ID__ \
   --mount type=bind,source="__PATH_HOST_WD__",target="__PATH_DOCKER_WD__" \
   __IMAGE_NAME__
