e126b984c666722d4f1b79bf0d9064054585a95b
Selim Raboudi <selim.raboudi@gmail.com>
2019-08-16 23:06:24 +0000

    correct: container mountpoint

--- deep/run.sh
+++ deep/run.sh
@@ -4,5 +4,5 @@ docker run \
   --gpus all \
   -p 8888:__JUPYTER_NOTEBOOK_PORT_ON_HOST__ \
   --rm --name __CONTAINER_NAME__ \
-  --mount type=bind,source="__PATH_HOST_WD__",target="__PATH_DOCKER_WD__" \
+  --mount type=bind,source="__PATH_HOST_WD__",target="__PATH_CONTAINER_MOUNTPOINT__" \
   __IMAGE_NAME__
