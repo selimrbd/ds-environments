2a23809a76d834d7a73cb02d5f8ab9dfb90d8659
Selim Raboudi <selim.raboudi@gmail.com>
2020-03-23 04:03:32 +0100

    reorganize file structure. Delete basic (keep only 1 ds env, the deep learning one, to avoid code redundancy)

--- basic/run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-docker run \
-  -it \
-  -p 8888:__JUPYTER_NOTEBOOK_PORT_ON_HOST__ \
-  --rm --name __CONTAINER_NAME__ \
-  --mount type=bind,source="__PATH_HOST_WD__",target="__PATH_CONTAINER_MOUNTPOINT__" \
-  __IMAGE_NAME__
